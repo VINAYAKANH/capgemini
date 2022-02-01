9b82739f3b48ee13ee29821405a5ffd475bdb976
Vinayaka NH <96069581+VINAYAKANH@users.noreply.github.com>
2022-02-01 09:04:19 +0530

    Create example.js

---
+++ example.js
@@ -0,0 +1,50 @@
+var xx=10;
+let xxx=7;
+var x=6,y=8,z=9;
+let xxh=9,xy=4,sz=5;
+var d=0.6464;
+let pi=3.64564;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(d);
+console.log(pi);
+var variable = 12345;
+let string="HelloWorld";
+var qwerty="qwerty";
+
+const vss=100;
+//vss=99;
+console.log(vss);
+
+//Java Script datatype
+// String,number,BigInt,354354135354635434n
+// Boolean,null,undefined,symbol and object
+//var string = 'Subham'       //String
+//var str='Majumdar'          //String
+//var BigInt=54152453255754424355748n;        //BigInt
+//var Boolean=true            //Boolean
+//var Bool=false              //Boolean
+//var a;                      //undefined
+//let value =Symbol('Mr.')    //Symbol
+//let val= Symbol('CJ')       //Symbol
+//let student={};             //Object
+
+let student ={
+    id:461675,
+    firstname:"Anil Kumar",
+    Lastname:"Gunapaneni",
+    batch:"JEE Full Stack"
+}
+console.log(student)
+// Note:=== Except object all the datatypes are primitive 
+console.log(typeof(student))
+
+// Java Script operators
+// 1. Assignment Operators : +=,-=,*=,/=,%=,**=
+// 2. Arithmatic Operators : +,-,*,/,**,%,--
+// 3. Comparison Operators : ==,!=,===,!==,<,>,<=,>=
+// 4. Logical Operators : &&,||,!
+// 5. Bitwise Operators: &,|,^,~,<<,>>,>>>
+// 6. String Operators: Concatenation                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+// 7. Other Operators: ,,?:,delete,typeof,inn,instance of 
